09bec159171dfc909f361f045d17e4669489a093
JosueGarcia04 <josueadrian042006@gmail.com>
2026-03-17 00:26:41 -0600

    cuerpo migracion aerolineas

--- database/migrations/2026_03_17_061628_create_aerolineas_table.php
+++ database/migrations/2026_03_17_061628_create_aerolineas_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('aerolineas', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('codigo_iata', 10)->unique();
+            $table->boolean('estado')->default(true);
             $table->timestamps();
         });
     }
