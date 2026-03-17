0db5d173f6123c2275eb00121718a768fbbe1152
JosueGarcia04 <josueadrian042006@gmail.com>
2026-03-17 00:18:29 -0600

    probando endpoint de registro

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 Route::post('/register', function (Request $request) {
-return response()->json([
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password)
+    ]);
+
+    return response()->json([
         'estado' => 'exito',
         'mensaje' => 'ruta funcionando correctamente',
-        'data' => $request->all()
+        'data' => $user
     ]);
 });
