a47641311b4bc34cd21df32b6e5611bcf98a1fae
JosueGarcia04 <josueadrian042006@gmail.com>
2026-03-20 00:19:05 -0600

    login funcionando

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/admin/dashboard', function () {
+    return '¡Bienvenido al panel de administración!';
+})->name('admin.dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
